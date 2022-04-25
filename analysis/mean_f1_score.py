7de14438dc115957a9edfb2cf477ea3c4a7a4593
vsantos <vsantos@ness.com.br>
2022-04-24 21:40:48 -0300

    mean f1 plots added

--- analysis/mean_f1_score.py
+++ analysis/mean_f1_score.py
@@ -1,21 +1,26 @@
 import numpy as np, matplotlib.pyplot as plt
 
+
 original_f1_mean = np.loadtxt('../complete_drug_response_data/F1_score.txt').mean()
+results_path = '../imputed_kernels_results'
 
-zero_10_mean = np.loadtxt('../imputed_kernels_results/zero/10/F1_score.txt').mean()
 
-print(original_f1_mean, zero_10_mean)
-
-data = {'Original': original_f1_mean, 'Zero 10': zero_10_mean}
-
-labels = data.keys()
-values = data.values()
-
-fig, ax = plt.subplots()
-bars = ax.bar(labels, values)
-ax.bar_label(bars)
-
-plt.xlabel("Iterations")
-plt.ylabel("Mean F1 Score")
-
-plt.savefig('./plots/mean_f1_score.png')
+for percentage in [10, 30, 50, 70]:
+    data = {'original': original_f1_mean}
+    
+    for technique in ['zero', 'mean', 'median', 'isvd', 'knn']:
+        data[technique] = np.loadtxt(results_path + f'/{technique}/{percentage}/F1_score.txt').mean()
+    
+    # Assembly plot
+    labels = data.keys()
+    values = data.values()
+    
+    fig, ax = plt.subplots()
+    bars = ax.bar(labels, values)
+    ax.bar_label(bars)
+    
+    plt.xlabel('Techniques')
+    plt.ylabel('Mean F1 Score')
+    plt.title(f'Mean F1 Score per Tecnique with {percentage}% NANs')
+    
+    plt.savefig(f'./plots/mean_f1_score/mean_f1_score_{percentage}.png')
