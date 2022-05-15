fbeadf8e261f05ec9545c43336906a9158d900d4
vsantos <vsantos@ness.com.br>
2022-05-15 17:21:14 -0300

    pearson and rsme charts added

--- analysis/gen-charts.py
+++ analysis/gen-charts.py
@@ -1,7 +1,7 @@
 import numpy as np, matplotlib.pyplot as plt
 
 
-original_f1_mean = np.loadtxt('../complete_drug_response_data/F1_score.txt').mean()
+original_f1_mean = np.loadtxt('../complete_drug_response_data/RMSE.txt').mean()
 results_path = '../imputed_kernels_results'
 
 
@@ -9,7 +9,7 @@ for percentage in [10, 30, 50, 70]:
     data = {'original': original_f1_mean}
     
     for technique in ['zero', 'mean', 'median', 'isvd', 'knn']:
-        data[technique] = np.loadtxt(results_path + f'/{technique}/{percentage}/F1_score.txt').mean()
+        data[technique] = np.loadtxt(results_path + f'/{technique}/{percentage}/RMSE.txt').mean()
     
     # Assembly plot
     labels = data.keys()
@@ -20,7 +20,7 @@ for percentage in [10, 30, 50, 70]:
     ax.bar_label(bars)
     
     plt.xlabel('Techniques')
-    plt.ylabel('Mean F1 Score')
-    plt.title(f'Mean F1 Score per Tecnique with {percentage}% NANs')
+    plt.ylabel('RSME')
+    plt.title(f'RSME per Tecnique with {percentage}% NANs')
     
-    plt.savefig(f'./plots/mean_f1_score/mean_f1_score_{percentage}.png')
+    plt.savefig(f'./plots/rsme/rsme_{percentage}.png')
